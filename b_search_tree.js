e9969a93b3d65e231d4899de09de179610aaa975
Tyler <tylerthomasallen@gmail.com>
2019-02-26 13:14:38 -0800

    Start bsearch tree

---
+++ b_search_tree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+}
